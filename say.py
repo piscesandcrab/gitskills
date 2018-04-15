4d9ea1061d93f43ae2f7b81d10cccd53b5e25abb
zhangjing <zj@debian>
2018-04-15 21:41:59 +0800

    begin coding

--- say.py
+++ say.py
@@ -1,1 +1,8 @@
-# hello
+#_*_ coding: UTF-8 _*_
+
+#from urllib import request
+import urllib.request
+
+if __name__ == "__main__":
+    respone = urllib.request.urlopen("https://github.com")
+    print(respone.read())
